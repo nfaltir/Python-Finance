5c3d96f9d39dbdeb7804f2cf435a1ce4df7058ed
nfaltir <nfaltir@protonmail.com>
2021-08-10 12:28:29 -0700

    refactored code, removed extra packages

--- graph.py
+++ graph.py
@@ -1,35 +1,21 @@
-
+from matplotlib import pyplot as plt
 import yfinance as yf
-import matplotlib.pyplot as plt
 import pandas as pd
-from matplotlib import style
-import datetime as dt
 
+plt.style.use("seaborn-dark")
 
-print("\n=========================Stock Price Grapher=========================\n")
-stock = str(input("Please enter the stock ticker:"))
+stock = yf.Ticker(input("enter stock ticker: "))
+print ("\n[1d, 5d, 1mo, 3mo, 6mo, 1y, 2y, 5y, 10y, ytd, max ]\n")
+time = input("enter period: ")
+priceHistory = stock.history(period=time)
+stockName = stock.info['shortName']
 
-stockObj = yf.Ticker(stock)
+#pandas DataFrame
+df = priceHistory
 
-
-
-y = "\n[***** 1d, 5d, 1mo, 3mo, 6mo, 1y, 2y, 5y, 10y, ytd, max============*****]\n"
-
-print(y)
-time = input("Please enter time frame?:")
-timeFrame = stockObj.history(period=time)
-
-
-
-df = pd.DataFrame(timeFrame)
-plt.style.use('bmh')
-plt.ylabel('Price ($USD)',fontsize=15)
-plt.xlabel('Date',fontsize=15)
-
-df['Close'].plot(figsize=(12,6))
-
-
-
-
-plt.title("Price History", fontsize=25)
+plt.figure()
+plt.plot(df['Close'])
+plt.title(stockName+ " Price history")
+plt.xlabel("time frame: " + time)
+plt.tight_layout()
 plt.show()
